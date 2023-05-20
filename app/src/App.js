75f31f67ae3dece6da641b8f09b75d389f9c8d84
iamjerriz <iamjerriz@gmail.com>
2023-05-20 18:57:54 +0800

    initial skeleton

---
+++ app/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+
+        <div id="left">
+        <h1>
+          Learn to code by <br/>
+          watching others
+        </h1>
+        <p className="left-section">
+            <span>See how experienced developers solve problems in real-time.</span>
+            <span>Watching scripted tutorial is great, but understanding how</span>
+            <span>developers think is invaluable</span>
+          </p>
+        </div>
+        <div id="right">
+          <button>sample</button>
+            <form className='right-form'>
+              <input></input>
+              <input></input>
+              <input></input>
+              <input></input>
+            </form>
+          <button>sample</button>
+
+        </div>
+         
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
